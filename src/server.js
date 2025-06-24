2c9d8f5aedb853dfd0f92b8ae5a05dfd40a323fc
tquang2410 <justicepencil@gmail.com>
2025-06-24 11:51:03 +0700

    #21 Config static file

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ require('dotenv').config() // Phải đặt ở đầu tiên
 const express = require('express')
 const path = require('path');
 const app = express()
-
+// Thêm dòng này để serve static files
+app.use(express.static('public'));
 // Lấy các biến môi trường từ file .env
 const port = process.env.PORT || 3000; // Mặc định là 3000 nếu không có trong .env
 const hostname = process.env.HOST_NAME || 'localhost';
@@ -14,6 +15,8 @@ console.log("check env", process.env)
 // cấu hình view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+// cấu hình thư mục chứa các file tĩnh
+app.use(express.static(path.join(__dirname, 'public')));
 
 // khai báo các route
 app.get('/', (req, res) => {
