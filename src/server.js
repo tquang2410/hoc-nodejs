994edc245789efbe2bd55a563ac9e642ba49a6d8
tquang2410 <justicepencil@gmail.com>
2025-06-24 10:49:38 +0700

    #19. ENV ( Environment Variables )

--- src/server.js
+++ src/server.js
@@ -1,21 +1,33 @@
+require('dotenv').config() // Phải đặt ở đầu tiên
+
 const express = require('express')
-// import express from 'express'
+const path = require('path');
 const app = express()
-const port = 3000
+
+// Lấy các biến môi trường từ file .env
+const port = process.env.PORT || 3000; // Mặc định là 3000 nếu không có trong .env
+const hostname = process.env.HOST_NAME || 'localhost';
+
+console.log("check env", process.env)
+
+
 // cấu hình view engine
-app.set('views', './views')
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
-// khai báo một route
+
+// khai báo các route
 app.get('/', (req, res) => {
     res.send('Hello World! Linga guliguli')
 })
-// khai báo một route với tham số
+
 app.get('/abc', (req, res) => {
     res.send('About me')
 })
+
 app.get('/contact', (req, res) => {
     res.render('sample.ejs')
 })
-app.listen(port, () => {
+
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
