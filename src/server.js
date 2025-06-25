e9578486a0bd25f10af50aed1e92f609c553746a
tquang2410 <justicepencil@gmail.com>
2025-06-25 09:44:31 +0700

    Add database.js for reuse

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const configViewEngine = require('./config/viewEngine');
+const {testMySQLConnection} = require('./config/database');
 // import các route
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2/promise');
+
 // Lấy các biến môi trường từ file .env
 const port = process.env.PORT || 3000; // Mặc định là 3000 nếu không có trong .env
 const hostname = process.env.HOST_NAME || 'localhost';
@@ -14,27 +15,9 @@ const hostname = process.env.HOST_NAME || 'localhost';
 // cấu hình view engine
 configViewEngine(app);
 // Sử dụng router
-app.use('/test', webRoutes);
+app.use('/', webRoutes);
 
-// Test MySQL connection
-async function testMySQLConnection() {
-    const connection = await mysql.createConnection({
-        host: 'localhost',
-        port: 3307, // Port mặc định của MySQL là 3306, nhưng nếu bạn đã thay đổi thì cần chỉnh lại
-        user: 'root', // Default user là 'root'
-        password: '123456',
-        database: 'hoidanit'
-    });
-    try {
-        const [results, fields] = await connection.query(
-            'select * from Users u '
-        );
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-    } catch (err) {
-        console.log(err);
-    }
-}
+
 testMySQLConnection();
 
 app.listen(port, hostname, () => {
